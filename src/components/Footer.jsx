7f4480b61c202da9b71624ddc6ef41609b03bb40
Viviana9110 <v.londono.naranjo@gmail.com>
2022-12-09 20:07:10 -0500

    deployment

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+
+
+const Footer = () => {
+  return (
+    <Footer className="py-10">
+      <p className="text-center font-bold">APV - Administrador de Pacientes de {''}
+      <span className="text-indigo-600">Veterinaria</span>
+      </p>
+      </Footer>
+  )
+}
+
+export default Footer
